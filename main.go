d8aa4c5373fa12bdbf2de81cefabec436ed22847
Ent <last_ent@outlook.com>
2017-11-22 17:22:50 +0100

    Using Prepare & Query.

--- main.go
+++ main.go
@@ -67,25 +67,112 @@ INSERT INTO
 `
 
 const allPublished = `
-SELECT e.rowid, *
+SELECT e.rowid, e.title, e.content, p.rowid
 FROM essays e
 JOIN published p
 ON p.essay_id = e.rowid;
 `
 
 const allEssays = `
-SELECT e.rowid, *
+SELECT e.rowid, e.title, e.content
 FROM essays e
 `
 
 const getEssay = `
-SELECT e.rowid, *
+SELECT e.rowid, e.title, e.content
 FROM essays e
 WHERE e.rowid = $1
 `
 
-func handleDBCalls(db *sql.DB, ch chan essay) {
+type DBInstance struct {
+	DB *sql.DB
+}
 
+func NewDBInstance(db *sql.DB) *DBInstance {
+	if _, err := db.Exec(crEssays); err != nil {
+		log.Fatal(err)
+	}
+	if _, err := db.Exec(crPublished); err != nil {
+		log.Fatal(err)
+	}
+
+	return &DBInstance{DB: db}
+}
+
+func (dbi *DBInstance) InsertPub(eID int) {
+	ips, err := dbi.DB.Prepare(insPub)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = ips.Exec(eID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := ips.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (dbi *DBInstance) GetAllPublished() {
+
+	aps, err := dbi.DB.Prepare(allPublished)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err := aps.Query()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var rowID, pubID int64
+		var title, content string
+
+		if err := rows.Scan(&rowID, &title, &content, &pubID); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(rowID, title, content, pubID)
+	}
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+// func a(db *sql.DB, dbi *DBInstance) {
+// 	ges, err := db.Prepare(getEssay)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+//
+// 	ies, err := db.Prepare(insEssay)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+//
+// 	aes, err := db.Prepare(allEssays)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+//
+// }
+
+func getAll(stmt *sql.Stmt) {
+
+}
+
+func publishEssay(db *sql.DB, eID int64) {
+
+	if _, err := db.Exec(insPub, eID); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleDBCalls(db *sql.DB, dbi *DBInstance, ch chan essay) {
 	for {
 		newEssay := <-ch
 		fmt.Println("New Essay:", newEssay)
@@ -100,14 +187,10 @@ func handleDBCalls(db *sql.DB, ch chan essay) {
 		if err != nil {
 			log.Fatal(err)
 		}
+		dbi.GetAllPublished()
 
-		if !newEssay.ToPublish {
-			continue
-		}
-
-		_, err = db.Exec(insPub, eID)
-		if err != nil {
-			log.Fatal(err)
+		if newEssay.ToPublish {
+			go publishEssay(db, eID)
 		}
 	}
 }
@@ -117,16 +200,8 @@ func dbSetup(ch chan essay) {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	if _, err := db.Exec(crEssays); err != nil {
-		log.Fatal(err)
-	}
-	if _, err := db.Exec(crPublished); err != nil {
-		log.Fatal(err)
-	}
-
-	go handleDBCalls(db, ch)
-
+	dbi := NewDBInstance(db)
+	go handleDBCalls(db, dbi, ch)
 }
 
 func editorHandler(w http.ResponseWriter, r *http.Request) {
