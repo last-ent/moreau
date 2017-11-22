5fcb5e484947f268dc3b5b37ebc24115251e5695
Ent <last_ent@outlook.com>
2017-11-22 12:36:18 +0100

    adding basic db

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -12,11 +13,21 @@ import (
 
 	"cloud.google.com/go/storage"
 	"golang.org/x/net/context"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const bucketName = "www.last-ent.com"
 
 var editorHtml []byte
+
+type essay struct {
+	Title     string
+	Content   string
+	ToPublish bool
+}
+
+var toDB chan essay
 
 func init() {
 	f, _ := os.Open("./editor.html")
@@ -26,6 +37,96 @@ func init() {
 	}
 
 	editorHtml = []byte(ed)
+
+	toDB = make(chan essay)
+
+	dbSetup(toDB)
+}
+
+const crEssays = `
+CREATE TABLE IF NOT EXISTS essays(
+	title TEXT NOT NULL,
+	content BLOB NOT NULL
+);`
+
+const crPublished = `
+CREATE TABLE IF NOT EXISTS published(
+	essay_id INTEGER
+);`
+
+const insEssay = `
+INSERT INTO
+	essays(title, content)
+	VALUES($1, $2);
+`
+
+const insPub = `
+INSERT INTO
+	published(essay_id)
+	VALUES($1);
+`
+
+const allPublished = `
+SELECT e.rowid, *
+FROM essays e
+JOIN published p
+ON p.essay_id = e.rowid;
+`
+
+const allEssays = `
+SELECT e.rowid, *
+FROM essays e
+`
+
+const getEssay = `
+SELECT e.rowid, *
+FROM essays e
+WHERE e.rowid = $1
+`
+
+func handleDBCalls(db *sql.DB, ch chan essay) {
+
+	for {
+		newEssay := <-ch
+		fmt.Println("New Essay:", newEssay)
+
+		res, err := db.Exec(
+			insEssay,
+			newEssay.Title,
+			newEssay.Content,
+		)
+		fmt.Printf("%#v\n%#v", res, err)
+		eID, err := res.LastInsertId()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if !newEssay.ToPublish {
+			continue
+		}
+
+		_, err = db.Exec(insPub, eID)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func dbSetup(ch chan essay) {
+	db, err := sql.Open("sqlite3", "./db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := db.Exec(crEssays); err != nil {
+		log.Fatal(err)
+	}
+	if _, err := db.Exec(crPublished); err != nil {
+		log.Fatal(err)
+	}
+
+	go handleDBCalls(db, ch)
+
 }
 
 func editorHandler(w http.ResponseWriter, r *http.Request) {
@@ -35,8 +136,15 @@ func editorHandler(w http.ResponseWriter, r *http.Request) {
 		go uploadPost(r.Form)
 		http.Redirect(w, r, "/", http.StatusMovedPermanently)
 
-	default:
+	case http.MethodGet:
 		w.Write(editorHtml)
+
+	default:
+		http.Error(
+			w,
+			http.StatusText(http.StatusMethodNotAllowed),
+			http.StatusMethodNotAllowed,
+		)
 	}
 }
 
@@ -44,7 +152,17 @@ func uploadPost(form url.Values) {
 	title := form["title"][0]
 	// form["content"] is a single string but better safe than sorry.
 	content := strings.Join(form["content"], "\n")
-	upload(title, strings.NewReader(content))
+	// upload(title, strings.NewReader(content))
+	toPub := false
+	if form["to_publish"][0] == "y" {
+		toPub = true
+	}
+
+	toDB <- essay{
+		Title:     title,
+		ToPublish: toPub,
+		Content:   content,
+	}
 }
 
 func main() {
